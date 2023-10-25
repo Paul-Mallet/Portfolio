07456b45a799daba734f862138f98b38d8d18861
Paul <paumallet76@gmail.com>
2023-10-25 12:48:56 +0200

    Added not play SVG to sound button

--- src/scripts/sound.js
+++ src/scripts/sound.js
@@ -1,4 +1,22 @@
+const audio = document.querySelector("audio");
 const sound_btn = document.querySelector(".right .hand_sound button");
-sound_btn.addEventListener("click", () => {
+sound_btn.addEventListener("click", toggle);
 
-});
+function toggle()
+{
+    const not = document.querySelector(".hand_sound button .not")
+    console.log(not.classList.value);
+    if (not.getAttribute("class") === "not play")
+    {
+        audio.pause();
+        not.classList.remove("play");
+        localStorage.removeItem("bg_sound");
+    }
+    else
+    {
+        audio.play();
+        audio.volume = 0.08;
+        not.classList.add("play");
+        localStorage.setItem("bg_sound", "play");
+    }
+}
