1a300e3f81c21c42ef8e002e9d196f00207d5acd
Paul <paumallet76@gmail.com>
2023-11-02 11:42:33 +0100

    Added if statement to resolve the back home page console error

--- src/scripts/little_js.js
+++ src/scripts/little_js.js
@@ -1,166 +1,112 @@
 document.addEventListener("astro:page-load", () => {
-    // FAQ DIV //
-    const faq_div_btns = document.querySelector(".right .faq .top .btns");
-    const faq_plus_btn = document.querySelector(".right .faq .top .btns .plus");
-    const faq_minus_btn = document.querySelector(".right .faq .top .btns .minus");
-    const faq_p = document.querySelector(".right .faq .content");
-    faq_div_btns.addEventListener("click", toggleDiv);
+    const href = window.location.href;
 
-    const nd = "not_display";
-    function toggleDiv()
+    if (href === "http://localhost:4321/projects")
     {
-        if (faq_minus_btn.classList != "minus not_display")
-        {
-            faq_minus_btn.classList.add(nd);
-            faq_plus_btn.classList.remove(nd);
-            faq_p.classList.add("hidden");
-        }
-        else
-        {
-            faq_minus_btn.classList.remove(nd);
-            faq_plus_btn.classList.add(nd);
-            faq_p.classList.remove("hidden");
-        }
-    }
+        // FAQ DIV //
+        const faq_div_btns = document.querySelector(".right .faq .top .btns");
+        const faq_plus_btn = document.querySelector(".right .faq .top .btns .plus");
+        const faq_minus_btn = document.querySelector(".right .faq .top .btns .minus");
+        const faq_p = document.querySelector(".right .faq .content");
+        faq_div_btns.addEventListener("click", toggleDiv);
 
-    // SHUTTLE_SPACE //
-    const container_right = document.querySelector(".right");
-    const s_div = document.querySelector(".right .shuttle_container");
-    const s_space = document.querySelector(".right .shuttle_space");
-    const s_space_text = document.querySelector(".right .shuttle_space p");
-    const rect = container_right.getBoundingClientRect();
-
-    let isMoving = false;
-    let x = 0;
-    let y = 0;
-    s_space.addEventListener("mousedown", (e) => {
-        isMoving = true;
-        s_space_text.style.display = "none";
-        s_div.style.zIndex = "11";
-    });
-
-    container_right.addEventListener("mousemove", (e) => {
-        if (isMoving)
+        const nd = "not_display";
+        function toggleDiv()
         {
-            x = e.clientX - Math.floor(rect.left + (s_space.offsetWidth / 2));
-            y = e.clientY - Math.floor(rect.top + (s_space.offsetHeight / 2));
-            if ((x >= (rect.width - (1 + (s_space.offsetWidth / 2)))
-            || x <= 1 - (s_space.offsetWidth / 2))
-            || (y >= (rect.height - (1 + (s_space.offsetHeight / 2)))
-            || y <= 1 - (s_space.offsetHeight / 2)))
+            if (faq_minus_btn.classList != "minus not_display")
             {
-                x = 64;
-                y = 370;
-            }
-            s_space.style.left = ((x / rect.width) * 100) + "%";
-            s_space.style.top = ((y / rect.height) * 100) + "%";
-        }
-    });
-
-    document.addEventListener("mouseup", () => {
-        if (isMoving)
-        {
-            isMoving = false;
-            s_space_text.style.display = "block";
-            s_div.style.zIndex = "0";
-        }
-    });
-
-    // TEST //
-    /*const div = document.querySelector("body div"); //right
-    const test = document.querySelector("body .test"); //space
-    const rect2 = div.getBoundingClientRect();
-
-    let isMoving2 = false;
-    let x2 = 0;
-    let y2 = 0;
-    test.addEventListener("mousedown", (e) => {
-        isMoving2 = true;
-    });
-
-    div.addEventListener("mousemove", (e) => {
-        if (isMoving2)
-        {
-            x2 = e.clientX - Math.floor(rect2.left + (test.offsetWidth / 2));
-            y2 = e.clientY - Math.floor(rect2.top + (test.offsetHeight / 2));
-            if ((x2 >= (rect2.width - 34) || x2 <= -32)
-            || (y2 >= (rect2.height - 19) || y2 <= -17))
-            {
-                x2 = 64;
-                y2 = 370;
-            }
-            test.style.left = ((x2 / rect2.width) * 100) + "%";
-            test.style.top = ((y2 / rect2.height) * 100) + "%";
-        }
-    });*/
-
-    /*function rotate()
-    {
-        if (x > prevX)
-        {
-            prevX = x;
-            test.style.rotate = "90deg";
-        }
-        else if (x < prevX)
-        {
-            prevX = x;
-            test.style.rotate = "270deg";
-        }
-        else if (y > prevY)
-        {
-            prevY = y;
-            test.style.rotate = "180deg"
-        }
-        else if (y < prevY)
-        {
-            prevY = y;
-            test.style.rotate = "0deg";
-        }
-    }*/
-
-    /*document.addEventListener("mouseup", () => {
-        if (isMoving2)
-        {
-            isMoving2 = false;
-        }
-    });*/
-
-    // COUNTER //
-    const score = document.querySelector(".right .stats .score span");
-    const target = document.querySelector(".right .target");
-    target.addEventListener("click", incrementer);
-
-    let nb = 1;
-    function incrementer()
-{
-    score.innerText = nb++;
-    target.style.top = Math.floor(Math.random() * (92 - 8) + 8) + '%';
-    target.style.left = Math.floor(Math.random() * (92 - 8) + 8) + '%';
-    }
-
-    // TIMEOUT //
-    const timeout = document.querySelector(".right .stats .timeout span");
-    const counter = setInterval(decrementer, 1000);
-
-    let sec = 9;
-    const zero = '0';
-    function decrementer()
-    {
-        if (sec >= 0)
-        {
-            if (sec >= 0 && sec <= 9)
-            {
-                timeout.innerText = zero + sec.toString();
+                faq_minus_btn.classList.add(nd);
+                faq_plus_btn.classList.remove(nd);
+                faq_p.classList.add("hidden");
             }
             else
             {
-                timeout.innerText = sec.toString();
+                faq_minus_btn.classList.remove(nd);
+                faq_plus_btn.classList.add(nd);
+                faq_p.classList.remove("hidden");
             }
-            sec--;
         }
-        else
+
+        // SHUTTLE_SPACE //
+        const container_right = document.querySelector(".right");
+        const s_div = document.querySelector(".right .shuttle_container");
+        const s_space = document.querySelector(".right .shuttle_space");
+        const s_space_text = document.querySelector(".right .shuttle_space p");
+        const rect = container_right.getBoundingClientRect();
+
+        let isMoving = false;
+        let x = 0;
+        let y = 0;
+        s_space.addEventListener("mousedown", (e) => {
+            isMoving = true;
+            s_space_text.style.display = "none";
+            s_div.style.zIndex = "11";
+        });
+
+        container_right.addEventListener("mousemove", (e) => {
+            if (isMoving)
+            {
+                x = e.clientX - Math.floor(rect.left + (s_space.offsetWidth / 2));
+                y = e.clientY - Math.floor(rect.top + (s_space.offsetHeight / 2));
+                if ((x >= (rect.width - (1 + (s_space.offsetWidth / 2)))
+                || x <= 1 - (s_space.offsetWidth / 2))
+                || (y >= (rect.height - (1 + (s_space.offsetHeight / 2)))
+                || y <= 1 - (s_space.offsetHeight / 2)))
+                {
+                    x = 38;
+                    y = 350;
+                }
+                s_space.style.left = ((x / rect.width) * 100) + "%";
+                s_space.style.top = ((y / rect.height) * 100) + "%";
+            }
+        });
+
+        document.addEventListener("mouseup", () => {
+            if (isMoving)
+            {
+                isMoving = false;
+                s_space_text.style.display = "block";
+                s_div.style.zIndex = "0";
+            }
+        });
+
+        // COUNTER //
+        const score = document.querySelector(".right .stats .score span");
+        const target = document.querySelector(".right .target");
+        target.addEventListener("click", incrementer);
+
+        let nb = 1;
+        function incrementer()
         {
-            clearInterval(counter);
+        score.innerText = nb++;
+        target.style.top = Math.floor(Math.random() * (92 - 8) + 8) + '%';
+        target.style.left = Math.floor(Math.random() * (92 - 8) + 8) + '%';
+        }
+
+        // TIMEOUT //
+        const timeout = document.querySelector(".right .stats .timeout span");
+        const counter = setInterval(decrementer, 1000);
+
+        let sec = 9;
+        const zero = '0';
+        function decrementer()
+        {
+            if (sec >= 0)
+            {
+                if (sec >= 0 && sec <= 9)
+                {
+                    timeout.innerText = zero + sec.toString();
+                }
+                else
+                {
+                    timeout.innerText = sec.toString();
+                }
+                sec--;
+            }
+            else
+            {
+                clearInterval(counter);
+            }
         }
     }
 });
