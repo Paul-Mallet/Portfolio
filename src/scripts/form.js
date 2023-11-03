e771a7c3dc81111c3e7cf685c02e8d4544045611
Paul <paumallet76@gmail.com>
2023-11-03 23:57:37 +0100

    Added input contraints validations for the form

---
+++ src/scripts/form.js
@@ -0,0 +1,74 @@
+document.addEventListener("astro:page-load", () => {
+    const href = window.location.href;
+
+    if (href === "http://localhost:4321/contact")
+    {
+        // VALIDATIONS //
+        const form = document.querySelector("form");
+        const name = document.querySelector("#name");
+        const job = document.querySelector("#job");
+        const email = document.querySelector(".others #email");
+        const message = document.querySelector(".others #message");
+        const errName = document.querySelector(".name_left span");
+        const errJob = document.querySelector(".job_right span");
+        const errEmail = document.querySelector(".others .email span");
+        const errMessage = document.querySelector(".others .message span");
+
+        const tabInput = [name, job, email, message];
+        const tabErr = [errName, errJob, errEmail, errMessage];
+        let i = 0;
+
+        name.addEventListener("input", () => {
+            if (name.validity.valid)
+            {
+                errName.innerText = "";
+                errName.className = "";
+            }
+        }, false);
+        job.addEventListener("input", () => {
+            if (job.validity.valid)
+            {
+                errJob.innerText = "";
+                errJob.className = "";
+            }
+        }, false);
+        email.addEventListener("input", () => {
+            if (email.validity.valid)
+            {
+                errEmail.innerText = "";
+                errEmail.className = "";
+            }
+        }, false);
+        message.addEventListener("input", () => {
+            if (message.validity.valid)
+            {
+                errMessage.innerText = "";
+                errMessage.className = "";
+            }
+        }, false);
+
+        form.addEventListener("submit", (e) => {
+            let j = 0;
+            while (j < tabInput.length)
+            {
+                if (tabInput[j].validity.valueMissing)
+                {
+                    tabErr[j].innerText = "Required";
+                    tabErr[j].className = "error";
+                }
+                else if (tabInput[2].validity.typeMismatch)
+                {
+                    tabErr[2].innerText = "Incorrect email";
+                    tabErr[2].className = "error";
+                }
+                else if(tabInput[2].validity.tooShort)
+                {
+                    tabErr[2].innerText = "Too short email";
+                    tabErr[2].className = "error";
+                }
+                j++;
+            }
+            e.preventDefault();
+        });
+    }
+});
