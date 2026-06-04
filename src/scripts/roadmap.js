4a588baecffbb9ed5507eb614194b98b884029d5
paul_mallet <paumallet76@gmail.com>
2026-06-04 20:17:43 +0200

    feat: add 1 story card to about section

--- src/scripts/roadmap.js
+++ src/scripts/roadmap.js
@@ -4,7 +4,6 @@ document.addEventListener("astro:page-load", () => {
     const href = window.location.href;
 
     if (href === "http://localhost:4321/about"
-        || href === "https://portfolio-pm.com/about"
         || href === "https://paul-mallet.netlify.app/about/")
     {
         // NEXT-BTN //
@@ -19,36 +18,80 @@ document.addEventListener("astro:page-load", () => {
             circle: all_circles,
             btn_nxt: all_btn_nxt,
             btn_prv: all_btn_prv,
-            animation_nxt: ["anim1", "anim2", "anim3", "anim4", "anim5", "anim6", "anim7"],
-            animation_prv: ["anim8", "anim9", "anim10", "anim11", "anim12", "anim13", "anim14"],
+            animation_nxt: ["anim1", "anim2", "anim3", "anim4", "anim5", "anim6", "anim7", "anim8"],
+            animation_prv: ["anim9", "anim10", "anim11", "anim12", "anim13", "anim14", "anim15", "anim16"],
         }
 
         let i = 0;
         roadObj.btn_nxt.forEach((btn) => {
             btn.addEventListener("click", () => {
                 path.classList.remove(roadObj.animation_prv[i]);
-                path.classList.remove(roadObj.animation_nxt[i-1]);
+                path.classList.remove(roadObj.animation_nxt[i - 1]);
+
                 i++;
-                path.classList.add(roadObj.animation_nxt[i-1]);
-                roadObj.box[i-1].classList.add("waiting");
-                gsap.fromTo(roadObj.box[i], {opacity: 0, y: 0}, { delay: 1.02, duration: .32, opacity: 1, y: 12, ease: "power1.out" });
+
+                path.classList.add(roadObj.animation_nxt[i - 1]);
+                roadObj.box[i - 1].classList.add("waiting");
+
+                gsap.fromTo(roadObj.box[i], {
+                    opacity: 0,
+                    y: 0
+                }, {
+                    delay: 1.02,
+                    duration: .32,
+                    opacity: 1,
+                    y: 12,
+                    ease: "power1.out"
+                });
+
                 roadObj.box[i].classList.remove("waiting");
                 roadObj.circle[i].classList.remove("waiting");
-                gsap.fromTo(roadObj.circle[i], { opacity: 0, scale: 0, transformOrigin: "center" },
-                {delay: .82, duration: .32, opacity: 1, scale: 1, transformOrigin: "center", ease: "back.out" });
+
+                gsap.fromTo(roadObj.circle[i], {
+                    opacity: 0,
+                    scale: 0,
+                    transformOrigin: "center"
+                }, {
+                    delay: .82,
+                    duration: .32,
+                    opacity: 1,
+                    scale: 1,
+                    transformOrigin: "center",
+                    ease: "back.out"
+                });
             });
         });
 
         roadObj.btn_prv.forEach((btn) => {
             btn.addEventListener("click", () => {
-                path.classList.remove(roadObj.animation_nxt[i-1]);
+                path.classList.remove(roadObj.animation_nxt[i - 1]);
                 path.classList.remove(roadObj.animation_prv[i]);
+
                 i--;
+
                 path.classList.add(roadObj.animation_prv[i]);
-                roadObj.box[i+1].classList.add("waiting");
-                gsap.fromTo(roadObj.box[i], { opacity: 0, y: -12 }, { delay: 1.02, duration: .32, opacity: 1, y: 0, ease: "power1.out" });
+                roadObj.box[i + 1].classList.add("waiting");
+
+                gsap.fromTo(roadObj.box[i], {
+                    opacity: 0,
+                    y: -12
+                }, {
+                    delay: 1.02,
+                    duration: .32,
+                    opacity: 1,
+                    y: 0,
+                    ease: "power1.out"
+                });
+
                 roadObj.box[i].classList.remove("waiting");
-                gsap.to(roadObj.circle[i+1], { duration: .32, opacity: 0, scale: 0, transformOrigin: "center", ease: "back.in" });
+
+                gsap.to(roadObj.circle[i + 1], {
+                    duration: .32,
+                    opacity: 0,
+                    scale: 0,
+                    transformOrigin: "center",
+                    ease: "back.in"
+                });
             });
         });
 
